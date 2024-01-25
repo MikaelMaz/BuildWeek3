66d01f7911785acebaee6ee98b570664b463d14d
Celeste Artuso <celesteartuso@hotmail.com>
2024-01-25 16:55:44 +0100

    responsive navbar

--- bw3-clone-linkedin/src/components/NavbarComponent.jsx
+++ bw3-clone-linkedin/src/components/NavbarComponent.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
+import {Container, Row, Col, Form, Nav, Navbar} from 'react-bootstrap';
 
 
 export default function NavbarComponent() {
@@ -10,59 +7,57 @@ export default function NavbarComponent() {
 
 <>
  
-
-
-
-
-
-
-
-
- <Navbar  data-bs-theme="light" className='defaultNavbar border-bottom border-tertiary p-1'>
-   <Container >
-   
-   <Navbar.Brand href="#" className='mx-1'>
-           <img className='logo' src="/icon.png" alt=""  />
-         </Navbar.Brand>
-         <Form className="d-flex w-50 " >
-           <Form.Control
-             type="search"
-             placeholder="Cerca"
-             className="me-5 input-bar"
-             aria-label="Search"
-           />
-         </Form>
-     <Nav className="me-auto top-nav p-0">
+ <Navbar  data-bs-theme="light" className='defaultNavbar bg-white border-bottom border-tertiary p-1 justify-content-center sticky-top'>
+   <Container fluid className='m-0 d-flex justify-content-between'>
+      <Navbar.Brand href="#" className='mx-1'>
+            <img className='logo' src="/icon.png" alt=""  />
+      </Navbar.Brand>
+    <Nav className="m-0 me-auto top-nav p-0 align-items-center">
+      <Nav.Link href="#home" className='mx-3'><div className='d-flex d-lg-none justify-content-center '>
+                   <i className="bi bi-search"></i>
+                 </div><span className='text-center d-none d-md-block d-lg-none'> Cerca</span></Nav.Link>
+      <Form className="d-flex w-50 d-none d-lg-block" >
+        <Form.Control
+          type="search"
+          placeholder="Cerca"
+          className="me-5 input-bar"
+          aria-label="Search"
+        />
+      </Form>
        <Nav.Link href="#home" className='mx-3'><div className='d-flex justify-content-center '>
                    <i className="bi bi-house-fill"></i>
-                 </div><span className='text-center'> Home</span></Nav.Link>
+                 </div>
+                 <span className='text-center d-none d-md-block'> Home</span></Nav.Link>
        <Nav.Link href="#features" className='mx-3'><div className='d-flex justify-content-center '>
                    <i className="bi bi-people-fill"></i>
-                 </div> Rete</Nav.Link>
+                 </div>
+                 <span className='text-center d-none d-md-block'> Rete</span> </Nav.Link>
        <Nav.Link href="#pricing" className='mx-3'><div className='d-flex justify-content-center '>
                    <i className="bi bi-briefcase-fill"></i>
-                 </div> Lavoro</Nav.Link>
+                 </div>
+                 <span className='text-center d-none d-md-block'> Lavoro</span> </Nav.Link>
        <Nav.Link href="#pricing" className='mx-3'><div className='d-flex justify-content-center '>
                    <i className="bi bi-chat-right-dots-fill"></i>
-                 </div> Messaggistica</Nav.Link>
+                 </div>
+                 <span className='text-center d-none d-md-block'> Messaggistica</span> </Nav.Link>
        <Nav.Link href="#pricing" className='mx-3'>  <div className='d-flex justify-content-center '>
                    <i className="bi bi-bell-fill"></i>
-                 </div> Notifiche</Nav.Link>
+                 </div>
+                 <span className='text-center d-none d-md-block'> Notifiche</span> </Nav.Link>
                  <Nav.Link href="#pricing" className='mx-3'>  <div className='d-flex justify-content-center '>
                    <i class="bi bi-person-circle"></i>
-                 </div> <span className='text-center'>tu</span></Nav.Link>
-       
+                 </div> 
+                 <span className='text-center d-none d-md-block'>tu</span></Nav.Link>
+       <div className='nav-right d-flex align-items-center justify-content-center'>
+        <Nav.Link href="#home" className=' mx-3'><div className='d-flex justify-content-center '>
+        <i class=" bi bi-list"></i>
+                  </div>
+                  <span className=' text-center d-none d-md-block'>Aziende</span></Nav.Link>
+        <Nav.Link href="#features" className='mx-3 '><div className='d-flex justify-content-center '>
+                  <span className='provapremium'> Prova Premium per 0 EUR</span> 
+                  </div> </Nav.Link>
+       </div>
      </Nav>
-     
-       <Nav.Link href="#home" className='mx-3'><div className='d-flex justify-content-center '>
-       <i class="bi bi-list"></i>
-                 </div>Aziende</Nav.Link>
-       <Nav.Link href="#features" className='mx-3 '><div className='d-flex justify-content-center '>
-                 <span className='provapremium'> Prova Premium per 0 EUR</span> 
-                 </div> </Nav.Link>
-       
-       
-     
    </Container>
  </Navbar>
  </>
