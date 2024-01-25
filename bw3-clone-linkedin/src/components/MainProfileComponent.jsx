8e11f1521aea01cb3b960c0f45ae80234e8cebaa
Simone Tonti <simone.tonti98@gmail.com>
2024-01-25 11:50:51 +0100

    index js icone

--- bw3-clone-linkedin/src/components/MainProfileComponent.jsx
+++ bw3-clone-linkedin/src/components/MainProfileComponent.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 
 export default function MainProfileComponent() {
   return (
-    <div>MainProfileComponent</div>
+    <Container className='bg-danger'>
+      <Container>
+      <i className="bi bi-camera-fill"></i>
+      </Container>
+      <Container></Container>
+      <Container></Container>
+      MainProfileComponent</Container>
   )
 }
