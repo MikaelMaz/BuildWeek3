d6162e007413eb4ae85c12af0bafdc80992fe8e1
Pierpaolo Giorgi <pierpaologiorgiiccs@gmail.com>
2024-01-29 16:47:06 +0100

    Conitnuo disperazione

--- bw3-clone-linkedin/src/components/Home/HomeProfileComponent.jsx
+++ bw3-clone-linkedin/src/components/Home/HomeProfileComponent.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container,Image } from 'react-bootstrap'
 import '../../homeProfile.css'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export default function HomeProfileComponent() {
+
+  const username = useSelector(state=>state.user)
+  
+  console.log(username)
+
   return (
     <>
     <div className='home-profile-global'>
@@ -10,14 +17,27 @@ export default function HomeProfileComponent() {
 
     <Container className='border rounded p-3 my-3'>
       <div className='border-bottom'>
-         <div>
-        {/* img-background */}
+         <div className='mainBg text-end position-absolute'>
+         <Image
+            src="https://t4.ftcdn.net/jpg/02/40/63/55/240_F_240635575_EJifwRAbKsVTDnA3QE0bCsWG5TLhUNEZ.jpg"
+            
+          >
+          
+          </Image>
+          {/* <div></div> */}
         </div>
         <div>
-          {/* immagine profilo */}
+        <Image 
+              src={username[0].image}
+              roundedCircle 
+              width={80} 
+              height={80} 
+              className = "border border-3 border-dark ms-3d-block">
+            </Image>
         </div>
         <div className='text-center mb-2'>
-          <h6 className='fw-semibold mb-0'>Ti diamo il benvenuto {/* nome utente */} </h6>
+          <h6 className='fw-semibold mb-0'>Ti diamo il benvenuto </h6>
+          <Link to={'/profile'} className='fw-semibold mb-0 d-block text-decoration-none text-black fs-6'>{username[0].name} !</Link>  {/* al click fa alla pagina profilo */}
           <a id='link-add-photo-home' className='text-primary text-decoration-none '>Aggiungi una foto</a>
         </div>
       </div>
@@ -34,12 +54,12 @@ export default function HomeProfileComponent() {
 
       <div className='border-bottom '>
         <p className='text-secondary mb-0 lh-1'>Accedi a strumenti e informazioni in esclusiva</p>
-         <p className='fw-semibold'> <i class="bi bi-square-fill text-warning"></i> Prova Premium per 0 EUR</p>
+         <p className='fw-semibold d-flex align-items-center'> <i className="bi bi-square-fill text-warning me-2"></i> Prova Premium per 0 EUR</p>
       </div>
 
       <div className='d-flex align-items-center align-content-center mt-2'>
         <i className="bi bi-bookmark-fill fs-6"></i>
-        <p className='fw-semibold mb-1 ms-2'>I miei elementi</p>
+        <p className='fw-semibold mb-0 ms-2'>I miei elementi</p>
       </div>
     </Container>
       
@@ -49,12 +69,12 @@ export default function HomeProfileComponent() {
        <p className='fw-semibold text-primary mb-2'>Gruppi</p> 
        <div className='d-flex justify-content-between align-items-center mb-2'>
         <p className='fw-semibold text-primary mb-0'>Eventi</p>
-        <button className='btn-plus-welcome align-self-start'><i class="bi bi-plus"></i></button>
+        <button className='btn-plus-welcome align-self-start'><i className="bi bi-plus"></i></button>
        </div>
         <p className='fw-semibold text-primary '>Hashtag seguiti</p>
       </div>
       <div>
-        <p className='fs-6 text-secondary text-center my-2 py-1 fw-semibold'>Scopri di più</p>
+        <p className='fs-6 text-secondary text-center my-2 py-1 fw-semibold text-decoration-none'>Scopri di più</p>
       </div>
     </Container>
     </div>
